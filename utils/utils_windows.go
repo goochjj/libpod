55d97ca2e376d945552de85da0925d9611bae086
Joseph Gooch <mrwizard@dok.org>
2020-06-18 22:27:56 +0000

    fix compat

--- utils/utils_windows.go
+++ utils/utils_windows.go
@@ -8,6 +8,10 @@ func RunUnderSystemdScope(pid int, slice string, unitName string) error {
 	return errors.New("not implemented for windows")
 }
 
+func MoveToCgroup2(cgroup string, subtree string) error {
+	return errors.New("not implemented for windows")
+}
+
 func GetPidCgroup(pid int) (string, error) {
 	return "", errors.New("not implemented for windows")
 }
